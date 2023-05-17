939623f570b40db8ada0623f85ce2d6854edb487
Angad Dhaliwal <000469061@student.vcc.ca>
2023-05-17 10:41:13 -0700

    first

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            Answer = new System.Windows.Forms.ListBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(43, 254);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(126, 58);
+            button1.TabIndex = 0;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(187, 254);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(126, 58);
+            button2.TabIndex = 1;
+            button2.Text = "x";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(43, 330);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(126, 58);
+            button3.TabIndex = 2;
+            button3.Text = "-";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button4.Location = new System.Drawing.Point(187, 330);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(126, 58);
+            button4.TabIndex = 3;
+            button4.Text = "÷";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(43, 59);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Number 1...";
+            textBox1.Size = new System.Drawing.Size(270, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(43, 101);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Number 2...";
+            textBox2.Size = new System.Drawing.Size(270, 23);
+            textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Answer
+            // 
+            Answer.FormattingEnabled = true;
+            Answer.ItemHeight = 15;
+            Answer.Location = new System.Drawing.Point(401, 59);
+            Answer.Name = "Answer";
+            Answer.Size = new System.Drawing.Size(331, 304);
+            Answer.TabIndex = 6;
+            Answer.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(Answer);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox Answer;
+    }
+}
